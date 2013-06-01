62563a139031e7426a5ca474c8dc92b50e821f81
Marcelo Goncalves <marcelocg@gmail.com>
2013-06-01 18:48:17 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bambah::Application.config.secret_token = 'ac86485542ae24133519ba7ae376afd048901a05314b68b47b1ac2eaec56658c0d28cf950a09beaae4a50faee1ff0727c0c206d0993c6d80a0f54c8cd8144cb1'
