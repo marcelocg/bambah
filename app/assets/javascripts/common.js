6bae598bb29df45ab610fa742610bd1bd55e596d
Marcelo Goncalves <marcelocg@gmail.com>
2013-06-04 11:23:29 +0000

    Ajax na tela de Lancamentos, ainda incompleto. Falta o retorno da consulta preencher a tabela na tela.

---
+++ app/assets/javascripts/common.js
@@ -0,0 +1,8 @@
+$('#loadingDiv')
+    .hide()  // hide it initially
+    .ajaxStart(function() {
+        $(this).show();
+    })
+    .ajaxStop(function() {
+        $(this).hide();
+    });
--- app/assets/javascripts/lancamentos.js
+++ app/assets/javascripts/lancamentos.js
@@ -12,7 +12,38 @@ $(function() {
   });
   
   $( "#spinMes" ).on( "spinstop", function( event, ui ) {
-    $( "#filtroLancamentos" ).submit();
+    $('#filtroLancamentos').submit();
   });
 
+  $('#filtroLancamentos').submit(loadLancamentos);
+  var buscarLancamentos = null;
+
+  function loadLancamentos(event) {
+    event.preventDefault();
+
+    if (buscarLancamentos) {
+      buscarLancamentos.abort();
+    }
+
+    var form = $(this).serialize();
+    buscarLancamentos = $.ajax('/lancamentos', {
+      data: form,
+      dataType: 'script',
+      cache: false,
+      timeout: 10000, //dez segundos
+      beforeSend: function(result) { 
+        $("#loadingDiv").modal('toggle');
+        $('#flash').hide();
+      },
+      complete: function(result) { 
+        $("#loadingDiv").modal('toggle');
+        buscarLancamentos = null;
+      },
+      error: function(result) {
+        if (result.statusText != "abort") {
+          $('#flash').show();
+        }
+      }
+    });
+  }
 });
